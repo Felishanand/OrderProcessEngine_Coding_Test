edfeac30866bd382d45963e0d0bf3b10a02aa3e6
Anandraj Srinivasan <anandraj@alligatortek.com>
2020-12-01 14:02:42 +0530

    3.Book Rule

---
+++ OrderProcessEngine.Core/BusinessRule/BookRule.cs
@@ -0,0 +1,25 @@
+﻿using OrderProcessEngine.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessEngine.Core.BusinessRule
+{
+    public class BookRule : ProcessOrder
+    {
+        internal BookRule(ProcessOrder processOrder, TypeOfRuleEnum typeOfRule) : base(processOrder, typeOfRule)
+        {
+        }
+
+        protected override TypeOfRuleEnum TypeOfRule { get; set; } = TypeOfRuleEnum.Book;
+
+        protected override List<TypeOfActionAgainstRule> Process()
+        {
+            var bookRuleProcess = new List<TypeOfActionAgainstRule>();
+            bookRuleProcess.Add(TypeOfActionAgainstRule.DuplicatePackaingSlip);
+            bookRuleProcess.Add(TypeOfActionAgainstRule.GenerateCommission);
+
+            return bookRuleProcess;
+        }
+    }
+}
---
+++ OrderProcessEngine.Test/BusinessRuleTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderProcessEngine.Core;
+using OrderProcessEngine.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessEngine.Test
+{
+    [TestClass]
+    public class BusinessRuleTest
+    {
+        private ExcuteRuleEngine excuteRuleEngine = null;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            excuteRuleEngine = new ExcuteRuleEngine();
+        }
+
+        [TestMethod]
+        public void HandlePhysicalProductOrder()
+        {
+            var result = excuteRuleEngine.ExcuteRule(TypeOfRuleEnum.PhysicalProduct);
+
+            Assert.IsTrue(result.Count == 2);
+
+            Assert.IsTrue(result.Contains(TypeOfActionAgainstRule.GenerateCommission));
+            Assert.IsTrue(result.Contains(TypeOfActionAgainstRule.GeneratePackagingSlip));
+        }
+
+        [TestMethod]
+        public void HandleBook()
+        {
+            var result = excuteRuleEngine.ExcuteRule(TypeOfRuleEnum.Book);
+
+            Assert.IsTrue(result.Count == 2);
+
+            Assert.IsTrue(result.Contains(TypeOfActionAgainstRule.GenerateCommission));
+            Assert.IsTrue(result.Contains(TypeOfActionAgainstRule.DuplicatePackaingSlip));
+        }
+
+        [TestMethod]
+        public void HandleVideo()
+        {
+            var result = excuteRuleEngine.ExcuteRule(TypeOfRuleEnum.LearningToSkiVideo);
+
+            Assert.IsTrue(result.Count == 1);
+            Assert.IsTrue(result.Contains(TypeOfActionAgainstRule.AddFreeAidVideoToPackagingSlip));
+        }
+    }
+}
